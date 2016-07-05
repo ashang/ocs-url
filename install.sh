c5e1b4c56cc1cf0806998a66a72ecfe5bc82721a
Akira Ohgaki <akiraohgaki@gmail.com>
2016-07-06 00:43:37 +0900

    Update install script

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ cd `dirname $0`
 sudo apt-get install python
 sudo apt-get install python-tk
 sudo apt-get install desktop-file-utils
-sudo install -m 755 src/xdgurl.py /usr/bin/xdgurl
-sudo install -m 644 src/xdgurl.desktop /usr/share/applications/xdgurl.desktop
+sudo install -D -m 755 src/xdgurl.py /usr/bin/xdgurl
+sudo install -D -m 644 src/xdgurl.desktop /usr/share/applications/xdgurl.desktop
 sudo update-desktop-database -q /usr/share/applications
