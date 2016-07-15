98a0b3bbb293e5a7465048246796d0b2e24521b5
Akira Ohgaki <akiraohgaki@gmail.com>
2016-07-15 23:02:40 +0900

    Remove package build script

--- pkg/arch/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd `dirname $0`
-
-tar -czvf /tmp/xdgurl.tar.gz ../../../xdgurl
-mv /tmp/xdgurl.tar.gz ./
-updpkgsums
-makepkg -s
--- pkg/ubuntu/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd `dirname $0`
-
-debuild -uc -us -b
-#fakeroot debian/rules clean
