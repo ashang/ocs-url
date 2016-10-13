3c8da09f7747258ba5c06605025a4c6f15559acb
Akira Ohgaki <akiraohgaki@gmail.com>
2016-10-13 21:52:22 +0900

    Remove test code

--- src/main.cpp
+++ src/main.cpp
@@ -39,9 +39,6 @@ int main(int argc, char *argv[])
 
     QStringList args = clParser.positionalArguments();
 
-    // This is for testing
-    args.append("xdg://install?url=https%3A%2F%2Fdl.opendesktop.org%2Fapi%2Ffiles%2Fdownload%2Fid%2F1460759361%2F129008-Faenza-Cupertino.tar.gz&type=icons&filename=129008-Faenza-Cupertino.tar.gz");
-
     if (args.size() != 1) {
         clParser.showHelp(1);
     }
