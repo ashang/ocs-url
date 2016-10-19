7ad3032fc4838a3f748a3bde15040953c9afa504
Akira Ohgaki <akiraohgaki@gmail.com>
2016-10-19 20:04:32 +0900

    Install build depends

--- pkg/build.sh
+++ pkg/build.sh
@@ -3,9 +3,7 @@
 cd `dirname $0`
 
 build_ubuntu() {
-    sudo apt install build-essential
-    sudo apt install qt5-default libqt5svg5-dev qtdeclarative5-dev
-    sudo apt install devscripts debhelper fakeroot
+    sudo apt install build-essential qt5-default libqt5svg5-dev qtdeclarative5-dev devscripts debhelper fakeroot
 
     mkdir ./build
     cp -r ../src ./build/
@@ -17,9 +15,7 @@ build_ubuntu() {
 }
 
 build_fedora() {
-    su -c 'dnf install make automake gcc gcc-c++ libtool'
-    su -c 'dnf install qt5-qtbase-devel qt5-qtsvg-devel qt5-qtdeclarative-devel'
-    su -c 'dnf install rpm-build'
+    su -c 'dnf install make automake gcc gcc-c++ libtool qt5-qtbase-devel qt5-qtsvg-devel qt5-qtdeclarative-devel rpm-build'
 
     tar -czvf /tmp/xdgurl.tar.gz ../../xdgurl
     mkdir ./build
@@ -31,8 +27,7 @@ build_fedora() {
 }
 
 build_arch() {
-    sudo pacman -S base-devel
-    sudo pacman -S qt5-base qt5-svg qt5-declarative
+    sudo pacman -S base-devel qt5-base qt5-svg qt5-declarative
 
     tar -czvf /tmp/xdgurl.tar.gz ../../xdgurl
     mkdir ./build
