a23f6a4fc64399a3abd84240299e71ed49e1380d
Akira Ohgaki <akiraohgaki@gmail.com>
2017-02-24 16:36:32 +0900

    Add default ts and qm files

---
+++ i18n/messages.ts
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>OcsUrlHandler</name>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="37"/>
+        <source>Invalid OCS-URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="141"/>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="162"/>
+        <source>Failed to save data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="148"/>
+        <source>The file has been downloaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="176"/>
+        <source>The file has been installed as program</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="180"/>
+        <source>The plasmoid has been installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="184"/>
+        <source>The plasma look and feel has been installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="188"/>
+        <source>The plasma desktop theme has been installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="192"/>
+        <source>The KWin effect has been installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="196"/>
+        <source>The KWin script has been installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="200"/>
+        <source>The KWin window switcher has been installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="203"/>
+        <source>The archive file has been extracted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="206"/>
+        <source>The file has been installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="210"/>
+        <source>Failed to installation</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../app/qml/main.qml" line="94"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="105"/>
+        <source>Download successfull</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="106"/>
+        <source>Installation successfull</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="107"/>
+        <location filename="../app/qml/main.qml" line="155"/>
+        <source>Validation error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="108"/>
+        <source>Network error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="109"/>
+        <source>Saving file failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="110"/>
+        <source>Installation failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="134"/>
+        <source>Downloading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="143"/>
+        <source>Do you want to download?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="146"/>
+        <source>Do you want to install?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="149"/>
+        <source>URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="150"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="151"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="156"/>
+        <source>Invalid OCS-URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
