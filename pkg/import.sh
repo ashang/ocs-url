39d1ae59ccbb0a4caaf318eb53eaf1651d96a279
Akira Ohgaki <akiraohgaki@gmail.com>
2017-02-26 18:17:26 +0900

    Use git to import library

--- pkg/import.sh
+++ pkg/import.sh
@@ -3,9 +3,9 @@
 PROJDIR="$(cd "$(dirname "${0}")/../" && pwd)"
 
 if [ ! -d "${PROJDIR}/lib/qtlib" ]; then
-    #git clone https://github.com/akiraohgaki/qtlib.git "${PROJDIR}/lib/qtlib"
-    curl -L -o qtlib-master.tar.gz https://github.com/akiraohgaki/qtlib/archive/master.tar.gz
-    tar -xzvf qtlib-master.tar.gz
-    mv qtlib-master "${PROJDIR}/lib/qtlib"
-    rm qtlib-master.tar.gz
+    #curl -L -o qtlib-master.tar.gz https://github.com/akiraohgaki/qtlib/archive/master.tar.gz
+    #tar -xzvf qtlib-master.tar.gz
+    #mv qtlib-master "${PROJDIR}/lib/qtlib"
+    #rm qtlib-master.tar.gz
+    git clone https://github.com/akiraohgaki/qtlib.git "${PROJDIR}/lib/qtlib"
 fi
