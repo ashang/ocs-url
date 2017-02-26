66d77e052989ba5e53315fb7b0439641a1ca8573
Akira Ohgaki <akiraohgaki@gmail.com>
2017-02-26 16:01:44 +0900

    Add import script

---
+++ pkg/import.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PROJDIR="$(cd "$(dirname "${0}")/../" && pwd)"
+
+if [ ! -d "${PROJDIR}/lib/qtlib" ]; then
+    #git clone https://github.com/akiraohgaki/qtlib.git "${PROJDIR}/lib/qtlib"
+    curl -L -o qtlib-master.tar.gz https://github.com/akiraohgaki/qtlib/archive/master.tar.gz
+    tar -xz qtlib-master.tar.gz
+    mv qtlib-master "${PROJDIR}/lib/qtlib"
+    rm qtlib-master.tar.gz
+fi
