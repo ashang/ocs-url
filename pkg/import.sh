9db415d94f4ba2cd748b25ee1e17675289963389
Akira Ohgaki <akiraohgaki@gmail.com>
2017-02-26 23:45:48 +0900

    Fix for git clone

--- pkg/import.sh
+++ pkg/import.sh
@@ -3,9 +3,5 @@
 PROJDIR="$(cd "$(dirname "${0}")/../" && pwd)"
 
 if [ ! -d "${PROJDIR}/lib/qtlib" ]; then
-    #curl -L -o qtlib-master.tar.gz https://github.com/akiraohgaki/qtlib/archive/master.tar.gz
-    #tar -xzvf qtlib-master.tar.gz
-    #mv qtlib-master "${PROJDIR}/lib/qtlib"
-    #rm qtlib-master.tar.gz
-    git clone https://github.com/akiraohgaki/qtlib.git "${PROJDIR}/lib/qtlib"
+    git clone https://github.com/akiraohgaki/qtlib.git -b master --single-branch --depth=1 "${PROJDIR}/lib/qtlib"
 fi
