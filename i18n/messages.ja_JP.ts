4c2b9ed86cefe7f6699a20e00d9e3fd4aa43225f
Akira Ohgaki <akiraohgaki@gmail.com>
2017-03-03 23:41:40 +0900

    Add ja_JP translation

---
+++ i18n/messages.ja_JP.ts
@@ -0,0 +1,170 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>OcsUrlHandler</name>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="37"/>
+        <source>Invalid OCS-URL</source>
+        <translation>無効なOCS-URL</translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="141"/>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="162"/>
+        <source>Failed to save data</source>
+        <translation>ファイルの保存に失敗</translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="148"/>
+        <source>The file has been downloaded</source>
+        <translation>ファイルがダウンロードされました</translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="176"/>
+        <source>The file has been installed as program</source>
+        <translation>ファイルがプログラムとしてインストールされました</translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="180"/>
+        <source>The plasmoid has been installed</source>
+        <translation>plasmoidがインストールされました</translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="184"/>
+        <source>The plasma look and feel has been installed</source>
+        <translation>plasma look and feelがインストールされました</translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="188"/>
+        <source>The plasma desktop theme has been installed</source>
+        <translation>plasma desktop themeがインストールされました</translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="192"/>
+        <source>The KWin effect has been installed</source>
+        <translation>KWin effectがインストールされました</translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="196"/>
+        <source>The KWin script has been installed</source>
+        <translation>KWin scriptがインストールされました</translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="200"/>
+        <source>The KWin window switcher has been installed</source>
+        <translation>KWin window switcherがインストールされました</translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="203"/>
+        <source>The archive file has been extracted</source>
+        <translation>圧縮ファイルが展開されました</translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="206"/>
+        <source>The file has been installed</source>
+        <translation>ファイルがインストールされました</translation>
+    </message>
+    <message>
+        <location filename="../app/src/handlers/ocsurlhandler.cpp" line="210"/>
+        <source>Failed to installation</source>
+        <translation>インストールに失敗</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../app/qml/main.qml" line="26"/>
+        <source>Download successfull</source>
+        <translation>ダウンロード成功</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="27"/>
+        <source>Installation successfull</source>
+        <translation>インストール成功</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="28"/>
+        <source>Validation error</source>
+        <translation>検証エラー</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="29"/>
+        <source>Network error</source>
+        <translation>通信エラー</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="30"/>
+        <source>Saving file failed</source>
+        <translation>ファイル保存失敗</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="31"/>
+        <source>Installation failed</source>
+        <translation>インストール失敗</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="56"/>
+        <source>Downloading</source>
+        <translation>ダウンロード中</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="66"/>
+        <source>Do you want to download?</source>
+        <translation>ダウンロードしますか?</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="69"/>
+        <source>Do you want to install?</source>
+        <translation>インストールしますか?</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="73"/>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="74"/>
+        <source>File</source>
+        <translation>ファイル</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="75"/>
+        <source>Type</source>
+        <translation>タイプ</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="80"/>
+        <source>Invalid OCS-URL</source>
+        <translation>無効なOCS-URL</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="96"/>
+        <location filename="../app/qml/main.qml" line="109"/>
+        <location filename="../app/qml/main.qml" line="125"/>
+        <source>Details</source>
+        <translation>詳細</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="98"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="100"/>
+        <location filename="../app/qml/main.qml" line="160"/>
+        <source>Cancel</source>
+        <translation>キャンセル</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="111"/>
+        <source>Open</source>
+        <translation>開く</translation>
+    </message>
+    <message>
+        <location filename="../app/qml/main.qml" line="116"/>
+        <location filename="../app/qml/main.qml" line="127"/>
+        <source>Close</source>
+        <translation>閉じる</translation>
+    </message>
+</context>
+</TS>
