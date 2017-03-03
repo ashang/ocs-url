474a9e61fa0e65e4f02a3559c08bb6d507de988c
Akira Ohgaki <akiraohgaki@gmail.com>
2017-03-03 23:24:57 +0900

    Add en_US translation

--- i18n/messages.en_US.ts
+++ i18n/messages.en_US.ts
@@ -6,68 +6,68 @@
     <message>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="37"/>
         <source>Invalid OCS-URL</source>
-        <translation type="unfinished"></translation>
+        <translation>Invalid OCS-URL</translation>
     </message>
     <message>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="141"/>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="162"/>
         <source>Failed to save data</source>
-        <translation type="unfinished"></translation>
+        <translation>Failed to save data</translation>
     </message>
     <message>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="148"/>
         <source>The file has been downloaded</source>
-        <translation type="unfinished"></translation>
+        <translation>The file has been downloaded</translation>
     </message>
     <message>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="176"/>
         <source>The file has been installed as program</source>
-        <translation type="unfinished"></translation>
+        <translation>The file has been installed as program</translation>
     </message>
     <message>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="180"/>
         <source>The plasmoid has been installed</source>
-        <translation type="unfinished"></translation>
+        <translation>The plasmoid has been installed</translation>
     </message>
     <message>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="184"/>
         <source>The plasma look and feel has been installed</source>
-        <translation type="unfinished"></translation>
+        <translation>The plasma look and feel has been installed</translation>
     </message>
     <message>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="188"/>
         <source>The plasma desktop theme has been installed</source>
-        <translation type="unfinished"></translation>
+        <translation>The plasma desktop theme has been installed</translation>
     </message>
     <message>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="192"/>
         <source>The KWin effect has been installed</source>
-        <translation type="unfinished"></translation>
+        <translation>The KWin effect has been installed</translation>
     </message>
     <message>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="196"/>
         <source>The KWin script has been installed</source>
-        <translation type="unfinished"></translation>
+        <translation>The KWin script has been installed</translation>
     </message>
     <message>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="200"/>
         <source>The KWin window switcher has been installed</source>
-        <translation type="unfinished"></translation>
+        <translation>The KWin window switcher has been installed</translation>
     </message>
     <message>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="203"/>
         <source>The archive file has been extracted</source>
-        <translation type="unfinished"></translation>
+        <translation>The archive file has been extracted</translation>
     </message>
     <message>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="206"/>
         <source>The file has been installed</source>
-        <translation type="unfinished"></translation>
+        <translation>The file has been installed</translation>
     </message>
     <message>
         <location filename="../app/src/handlers/ocsurlhandler.cpp" line="210"/>
         <source>Failed to installation</source>
-        <translation type="unfinished"></translation>
+        <translation>Failed to installation</translation>
     </message>
 </context>
 <context>
@@ -75,96 +75,96 @@
     <message>
         <location filename="../app/qml/main.qml" line="26"/>
         <source>Download successfull</source>
-        <translation type="unfinished"></translation>
+        <translation>Download successfull</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="27"/>
         <source>Installation successfull</source>
-        <translation type="unfinished"></translation>
+        <translation>Installation successfull</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="28"/>
         <source>Validation error</source>
-        <translation type="unfinished"></translation>
+        <translation>Validation error</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="29"/>
         <source>Network error</source>
-        <translation type="unfinished"></translation>
+        <translation>Network error</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="30"/>
         <source>Saving file failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Saving file failed</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="31"/>
         <source>Installation failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Installation failed</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="56"/>
         <source>Downloading</source>
-        <translation type="unfinished"></translation>
+        <translation>Downloading</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="66"/>
         <source>Do you want to download?</source>
-        <translation type="unfinished"></translation>
+        <translation>Do you want to download?</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="69"/>
         <source>Do you want to install?</source>
-        <translation type="unfinished"></translation>
+        <translation>Do you want to install?</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="73"/>
         <source>URL</source>
-        <translation type="unfinished"></translation>
+        <translation>URL</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="74"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>File</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="75"/>
         <source>Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Type</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="80"/>
         <source>Invalid OCS-URL</source>
-        <translation type="unfinished"></translation>
+        <translation>Invalid OCS-URL</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="96"/>
         <location filename="../app/qml/main.qml" line="109"/>
         <location filename="../app/qml/main.qml" line="125"/>
         <source>Details</source>
-        <translation type="unfinished"></translation>
+        <translation>Details</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="98"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="100"/>
         <location filename="../app/qml/main.qml" line="160"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Cancel</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="111"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Open</translation>
     </message>
     <message>
         <location filename="../app/qml/main.qml" line="116"/>
         <location filename="../app/qml/main.qml" line="127"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Close</translation>
     </message>
 </context>
 </TS>
